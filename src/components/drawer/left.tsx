2a7a24a308f8d5d73d949c130f449cc25b1e46e2
Thomas Carling <thomasmcarling@gmail.com>
2024-05-20 20:59:13 +0100

    remove line in drawer that looks too much like a scrollbar

--- src/components/drawer/left.tsx
+++ src/components/drawer/left.tsx
@@ -19,9 +19,8 @@ export const LeftDrawer = ({ children }: { children: ReactNode }) => (
     </Trigger>
     <Portal>
       <Overlay className="fixed inset-0 z-50 bg-black/80 sm:hidden" />
-      <Content className="fixed inset-y-0 left-0 z-50 flex w-auto rounded-r-[10px] border border-zinc-200 bg-white py-10 dark:border-zinc-800 dark:bg-zinc-950 sm:hidden">
+      <Content className="fixed inset-y-0 left-0 z-50 flex w-auto rounded-r-[10px] border border-zinc-200 bg-white px-3 py-10 dark:border-zinc-800 dark:bg-zinc-950 sm:hidden">
         {children}
-        <div className="my-auto mr-4 h-32 w-2 rounded-full bg-zinc-100 dark:bg-zinc-800" />
       </Content>
     </Portal>
   </DrawerRoot>
