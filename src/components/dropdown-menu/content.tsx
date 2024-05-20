5cb6baf92771caadb515ea99e14f3142ee5df769
Thomas Carling <thomasmcarling@gmail.com>
2024-05-20 20:08:03 +0100

    close theme menu immediately on change

--- src/components/dropdown-menu/content.tsx
+++ src/components/dropdown-menu/content.tsx
@@ -8,10 +8,9 @@ export const DropdownMenuContent = ({ children }: { children: ReactNode }) => (
       align="end"
       className={cn(
         "data-[state=open]:duration-300 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-90",
-        "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-90",
-        "data-[side=bottom]:slide-in-from-top-4 ",
+        "data-[side=bottom]:slide-in-from-top-4",
         "z-50 flex w-36 flex-col items-stretch overflow-hidden rounded-md border border-zinc-200 p-1",
-        " mt-1 bg-white text-zinc-950 shadow-md dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-50",
+        "mt-1 bg-white text-zinc-950 shadow-md dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-50",
       )}
     >
       {children}
