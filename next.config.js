ad55aee9f03374cf6b4019dc8d0a9aa0adab4d8a
Thomas Carling <thomasmcarling@gmail.com>
2025-01-30 20:24:08 +0000

    opt into react compiler

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  experimental: {
+    reactCompiler: true,
+  },
+};
 
 export default config;
